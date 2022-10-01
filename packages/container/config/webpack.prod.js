cab965da7abedc11f49fc79a36001d6166479b4e
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2022-10-01 17:11:59 +0300

    fixed public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -12,7 +12,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: 'container/latest/',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
