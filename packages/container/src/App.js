14e815b7e6e0da579335e04c6661426837b9ad46
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2022-10-01 17:05:23 +0300

    added auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
@@ -13,14 +13,19 @@ const generateClassName = createGenerateClassName({
 });
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
-        <Header />
+        <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
         <Suspense fallback={<Progress />}>
           <Switch>
-            <Route path="/auth" component={AuthLazy} />
-            <Route path="/" component={MarketingLazy} />
+            <Route path="/auth">
+              <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+            </Route>
+            <Route path="/">
+              <MarketingLazy />
+            </Route>
           </Switch>
         </Suspense>
       </BrowserRouter>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 import { mount } from 'auth/AuthApp';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -14,6 +14,7 @@ export default () => {
       onNavigate({ pathname: nextPathname }) {
         if (pathname !== nextPathname) history.push(nextPathname);
       },
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
