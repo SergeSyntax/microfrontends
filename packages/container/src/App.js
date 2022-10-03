b72316b2a0650dca00943c5871fb9be593dbce56
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2022-10-03 22:18:12 +0300

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -24,6 +25,7 @@ function App() {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard" component={DashboardLazy} />
               <Route path="/">
                 <MarketingLazy />
               </Route>
